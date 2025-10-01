5f404812b460d8ae31fe6e9bac26564b09373321
vihashah <viha.shah@sjsu.edu>
2025-10-01 10:07:18 -0700

    mini skeleton

--- app/home/user/page.tsx
+++ app/home/user/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-// this will be the landing page
-export default function Home() {
+// this will be the 
+export default function UserDashboard() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div>
-        Welcome to a Food Store App
+        
         {/* Need to add a carousel here for sample products */}
         {/* Links to different pages for our router */}
+        Here we will have a list of products in cards and allow the user to select items
       </div>
     </div>
   );
